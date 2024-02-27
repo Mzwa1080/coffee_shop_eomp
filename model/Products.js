d8b366a997a850dfe7ed4184c877f6fd66a0d238
HaanimP <haanimpietersen@gmail.com>
2024-02-27 14:32:25 +0200

    added some few changes

--- model/Products.js
+++ model/Products.js
@@ -1,83 +1,42 @@
- import {connection as db} from "../config/index.js"
- class Products{
+import {connection as db} from '../config/index.js'
+class Products{
     fetchProducts(req,res){
-        const qry =`
-        SELECT prodID,prodName,prodQuantity, productAmount, description, prodUrl
-        FROM products;  `
-        db.query(qry, (err, results) => {
-            if (err) throw err;
+        const qry = `
+        select prodID, prodName,prodQuality,prodAmount, userID
+         from Products;`
+         db.query(qry, (err, results)=>{
+            if(err) throw err
             res.json({
-              status: res.statusCode,
-              results,
-            });
-          }); 
+                status : res.statusCode,
+                results
+            })
+         })
     }
     fetchProduct(req,res){
-        const qry =`
-        SELECT prodID,prodName,prodQuantity, productAmount,userID
+        const qry = `
+        select prodID,prodName,prodQuantity, productAmount,userID
         FROM products WHERE prodID = ${req.params.id};`
-        db.query(qry, (err, result) => {
-            if (err) throw err;
+         db.query(qry, (err, results)=>{
+            if(err) throw err
             res.json({
-              status: res.statusCode,
-              result,
-            });
-          }); 
+                status : res.statusCode,
+                results
+            })
+         })
     }
     addProduct(req,res){
-        const qry=` INSERT INTO products SET ?;`
-        let data = req.body
-        db.query(qry,[data], (err)=>{
-          if(err) throw err
-          res.json({
-            status: res.statusCode,
-            msg:'new product was added'
-          })
-
-
+        const qry = `
+            insert into products set ?;
+        `
+        db.query(qry, [data], (err)=>{
+            if(err) throw err
+            res.json({
+                status : res.statusCode,
+                msg : "New product was added"
+            })
         })
     }
-    deleteProducts(req,res){
-      const qry=`DELETE FROM products ;`
-
-      db.query(qry, (err)=>{
-        if(err) throw err
-        
-        res.json({
-          status: res.statusCode,
-          msg:'Product was delete!'
-        })
-
-      })
-  }
-  deleteProduct(req,res){
-    const qry=`DELETE FROM products WHERE prodID= ? ;`
-    db.query(qry, [req.params.id], (err)=>{
-        if(err) throw err
-        res.json({
-            status: res.statusCode,
-            msg:'Product is deleted!'
-        })
-    })
 }
-async updateProduct(req,res){
-  const qry=`
-  UPDATE products 
-  SET ?
-  WHERE prodID = ?;`
-  db.query(qry, [req.body, req.params.id], (err)=>{
-      if(err) throw err
-      
-      res.json({
-          status: res.statusCode,
-          msg:'Product is updated!'
-      })
-  })
-}
-
- 
- }
- export{
+export{
     Products
-    
- }
+}
