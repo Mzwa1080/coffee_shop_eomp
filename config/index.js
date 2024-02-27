65458db2c714907c9411ace00e7962140753e362
HaanimP <haanimpietersen@gmail.com>
2024-02-27 14:51:08 +0200

    increased the connections

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ let connection=createPool({
     user: process.env.DB_UserName,
     password: process.env.DB_UserPass,
     multipleStatements:true,
-    connectionLimit:30
+    connectionLimit: 60
 })
 export{
 connection
