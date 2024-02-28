0851f56fca682ee6d8b4456125cffc2940ee6627
HaanimP <haanimpietersen@gmail.com>
2024-02-28 10:59:11 +0200

    fixed authentication

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { products } from '../model/index.js'
+import { verifyToken } from '../middleware/AuthenticateUser.js'
 
 const productRouter=express.Router()
 
 // Fetch all products
-productRouter.get('/', (req, res) => {
+productRouter.get('/', verifyToken, (req, res) => {
     try {
         products.fetchProducts(req, res);
     } catch (e) {
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 config()
 
 const app=express()
-const port = +process.env.PORT || 4002
+const port = +process.env.PORT || 4007
 
 //Middleware
 app.use((req,res,next)=>{
