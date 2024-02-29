da04d29fb27b000f3557a6210e77a55b13812bf8
Mzwamadoda Shawn Louw <32576740+Mzwa1080@users.noreply.github.com>
2024-02-29 12:07:43 +0200

    products

--- model/Products.js
+++ model/Products.js
@@ -1,29 +1,28 @@
  import {connection as db} from "../config/index.js"
  class Products{
-    fetchProducts(req,res){
-        const qry =`
+        fetchProducts(req,res){
+        const qry = `
         select prodID,prodName,prodQuantity, productAmount,description, prodUrl
-        FROM products;  `
-        db.query(qry, (err, results) => {
-            if (err) throw err;
+        FROM products;`
+         db.query(qry, (err, results)=>{
+            if(err) throw err
             res.json({
-              status: res.statusCode,
-              results
-            });
-          }); 
+                status : res.statusCode,
+                results
+            })
+         })
     }
     fetchProduct(req,res){
-        const qry =`
+        const qry = `
         select prodID,prodName,prodQuantity, productAmount,description, prodUrl
-        FROM products WHERE prodID=${req.params.id};
-        `
-        db.query(qry, (err, result) => {
-            if (err) throw err;
+        FROM products WHERE prodID = ${req.params.id};`
+         db.query(qry, (err, results)=>{
+            if(err) throw err
             res.json({
-              status: res.statusCode,
-              result : result[0]
-            });
-          }); 
+                status : res.statusCode,
+                results
+            })
+         })
     }
     addProduct(req,res){
         const qry=` INSERT INTO products SET ?;`
