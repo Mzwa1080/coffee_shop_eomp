a752d031b6a28b545d21aeb73fb04242f03517f0
Mzwamadoda Shawn Louw <32576740+Mzwa1080@users.noreply.github.com>
2024-02-29 11:31:32 +0200

    Updated products functionallity

--- model/Products.js
+++ model/Products.js
@@ -1,42 +1,44 @@
-import {connection as db} from '../config/index.js'
-class Products{
+ import {connection as db} from "../config/index.js"
+ class Products{
     fetchProducts(req,res){
-        const qry = `
-        select prodID,prodName,prodQuantity, productAmount,description, prodUrl
-        FROM products;`
-         db.query(qry, (err, results)=>{
-            if(err) throw err
+        const qry =`
+        SELECT prodID,prodName,prodQuantity, productAmount,userID
+        FROM products;  `
+        db.query(qry, (err, results) => {
+            if (err) throw err;
             res.json({
-                status : res.statusCode,
-                results
-            })
-         })
+              status: res.statusCode,
+              results,
+            });
+          }); 
     }
     fetchProduct(req,res){
-        const qry = `
-        select prodID,prodName,prodQuantity, productAmount,description, prodUrl
-        FROM products WHERE prodID = ${req.params.id};`
-         db.query(qry, (err, result)=>{
-            if(err) throw err
+        const qry =`
+        SELECT prodID,prodName,prodQuantity, productAmount,userID
+        FROM products WHERE prodID = ${req.params.id}
+        `
+        db.query(qry, (err, result) => {
+            if (err) throw err;
             res.json({
-                status : res.statusCode,
-                result : result[0]
-            })
-         })
+              status: res.statusCode,
+              result : result[0],
+            });
+          }); 
     }
     addProduct(req,res){
-        const qry = `
-            insert into products set ?;
-        `
-        db.query(qry, [data], (err)=>{
-            if(err) throw err
-            res.json({
-                status : res.statusCode,
-                msg : "New product was added"
-            })
+        const qry=` INSERT INTO products SET ?;`
+        let data = req.body
+        db.query(qry,[data], (err)=>{
+          if(err) throw err
+          res.json({
+            status: res.statusCode,
+            msg:'new product was added'
+          })
+
+
         })
     }
- deleteProducts(req,res){
+    deleteProducts(req,res){
       const qry=`DELETE FROM products ;`
 
       db.query(qry, (err)=>{
@@ -50,15 +52,19 @@ class Products{
       })
   }
   deleteProduct(req,res){
-    const qry=`DELETE FROM products WHERE prodID=  ${req.params.id} ;`
-    db.query(qry, [req.params.id], (err)=>{
-        if(err) throw err
-        res.json({
-            status: res.statusCode,
-            msg:'Product is deleted!'
-        });
-    });
-}
+    const qry=`DELETE FROM products WHERE prodID=${req.params.id} ;`
+    // const user = req.body
+  
+    db.query(qry, (err)=>{
+  
+      if(err) throw err
+      res.json({
+        status: res.statusCode,
+        msg:'Product is deleted!'
+      })
+  
+    })
+  }
   async updateProduct(req,res){
     const qry=`
     UPDATE products 
@@ -75,8 +81,10 @@ class Products{
   
     })
   }
-}
 
-export{
+ 
+ }
+ export{
     Products
-}
+    
+ }
